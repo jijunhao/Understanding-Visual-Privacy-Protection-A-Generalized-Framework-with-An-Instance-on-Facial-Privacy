f5b4537804c973e02b71413a7b70f349d8f9b862
jijunhao <2818657803@qq.com>
2023-04-19 20:18:32 +0800

    add main

--- models/vit.py
+++ models/vit.py
@@ -129,18 +129,20 @@ class ViT(nn.Module):
 
         return self.mlp_head(x)
 
-v = ViT(
-    image_size = 256,
-    patch_size = 32,
-    num_classes = 1000,
-    dim = 1024,
-    depth = 6,
-    heads = 16,
-    mlp_dim = 2048,
-    dropout = 0.1,
-    emb_dropout = 0.1
-)
+if __name__ == '__main__':
 
-img = torch.randn(1, 3, 256, 256)
+    v = ViT(
+        image_size = 256,
+        patch_size = 32,
+        num_classes = 1000,
+        dim = 1024,
+        depth = 6,
+        heads = 16,
+        mlp_dim = 2048,
+        dropout = 0.1,
+        emb_dropout = 0.1
+    )
 
-preds = v(img)
+    img = torch.randn(1, 3, 256, 256)
+
+    preds = v(img)
