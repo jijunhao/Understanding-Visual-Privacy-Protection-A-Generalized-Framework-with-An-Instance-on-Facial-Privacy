e24facb85dd4bf858bc2457450de092a10a17e49
jijunhao <2818657803@qq.com>
2023-07-20 19:07:06 +0800

    修复数据集bug

--- Data_preprocessing/caption2json.py
+++ Data_preprocessing/caption2json.py
@@ -28,10 +28,11 @@ for i in trange(30000):
         joined_lines = ' '.join([line.strip() for line in lines])
 
         # 去除重复的句子
-        unique_strings = list(set(joined_lines.split(". ")))
+        unique_strings = list(set(joined_lines[:-1].split(". ")))
+
 
         # 重新拼接
-        joined_lines = '. '.join([line.strip() for line in unique_strings])
+        joined_lines = '. '.join([line.strip() for line in unique_strings])+'.'
 
 
         # 将拼接后的结果转换成JSON格式
@@ -41,6 +42,8 @@ for i in trange(30000):
 json_filename = os.path.join(folder_save, "captions.json")
 with open(json_filename, 'w') as json_file:
     json.dump(text, json_file, indent=4)
+
+
 
 
 
